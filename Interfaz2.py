d42d7144716d7e3195b0d0677737705f71474f8a
dam <daapariciopereira2@danielcastelao.org>
2025-11-06 10:07:36 +0100

    general 0.3

--- Interfaz2.py
+++ Interfaz2.py
@@ -54,9 +54,14 @@ class InterfazEjemplo(Gtk.Window):
         vbox_list = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         frame_list.add(vbox_list)
 
-        liststore = Gtk.ListStore(str)
-        for item in ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]:
-            liststore.append([item])
+        liststore = Gtk.ListStore(str,str)
+        liststore.append(('Saray',"11111K"))
+        liststore.append(('Daniel',"22222R"))
+        liststore.append(('Karli',"33333G"))
+        liststore.append(('manuel',"44444D"))
+        liststore.append(('Chema',"55555L"))
+        liststore.append(('Diego',"66666P"))
+
 
         treeview = Gtk.TreeView(model=liststore)
         renderer = Gtk.CellRendererText()
@@ -70,19 +75,29 @@ class InterfazEjemplo(Gtk.Window):
 
         # --- Sección derecha: Radio buttons + Botón ---
         vbox_controls = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        hbox_left_main.pack_start(vbox_controls, False, False, 0)
+        hbox_left_main.add(vbox_controls)
 
         radio1 = Gtk.RadioButton.new_with_label_from_widget(None, "RadioButton1")
+        radio1.connect("toggled",self.on_rbt_toggled,"1")
+
         radio2 = Gtk.RadioButton.new_with_label_from_widget(radio1, "RadioButton2")
+        radio1.connect("toggled",self.on_rbt_toggled,"2")
+
         radio3 = Gtk.RadioButton.new_with_label_from_widget(radio1, "RadioButton3")
+        radio1.connect("toggled",self.on_rbt_toggled,"3")
+
         radio4 = Gtk.RadioButton.new_with_label_from_widget(radio1, "InactiveRadio")
+        radio1.connect("toggled",self.on_rbt_toggled,"4")
+
         radio4.set_sensitive(False)
 
-        for r in [radio1, radio2, radio3, radio4]:
-            vbox_controls.pack_start(r, False, False, 0)
+        vbox_controls.add(radio1)
+        vbox_controls.add(radio2)
+        vbox_controls.add(radio3)
+        vbox_controls.add(radio4)
 
         button = Gtk.Button(label="Button")
-        vbox_controls.pack_start(button, False, False, 0)
+        vbox_controls.pack_start(button, False, False, 2)
 
         # === PANEL DERECHO (Notebook con tabs) ===
         notebook = Gtk.Notebook()
@@ -112,19 +127,19 @@ class InterfazEjemplo(Gtk.Window):
         grid_bottom.set_row_spacing(4)
         grid_bottom.set_column_spacing(4)
 
-
-
-
-
         entry_text = Gtk.Entry()
         entry_text.set_text("TextField")
         entry_pass = Gtk.Entry()
         entry_pass.set_visibility(False)
-        combo = Gtk.ComboBoxText()
-        for i in ["Item 1", "Item 2", "Item 3"]:
-            combo.append_text(i)
-        combo.set_active(0)
-        text_area = Gtk.TextView()
+        combo = Gtk.ComboBox()
+        combo.set_model(model=liststore)
+        renderer2 = Gtk.CellRendererText()
+        combo.pack_start(renderer2,False)
+        combo.add_attribute(renderer2,"text",0)
+        combo.connect("changed",self.on_combo_changed)
+
+
+        self.text_area = Gtk.TextView()
 
 
 
@@ -133,13 +148,32 @@ class InterfazEjemplo(Gtk.Window):
         grid_bottom.attach(entry_pass, 0, 1, 1, 1)
         grid_bottom.attach(combo, 0, 2, 1, 1)
         grid_bottom.attach(Gtk.Label(label="TextArea"), 1, 0, 1, 1)
-        grid_bottom.attach(text_area, 1, 1, 100, 70)
+        grid_bottom.attach(self.text_area, 1, 1, 85, 79)
 
         grid.attach(grid_bottom,0,Gtk.PositionType.BOTTOM,1,1)
 
         hbox_panel.add(grid)
 
         self.show_all()
+
+    def on_rbt_toggled(self,button, numero):
+        estado= button.get_active()
+        if estado:
+            self.escribir_textView("Botón " + numero + " presionado\n")
+
+    def escribir_textView(self, texto):
+        buffer = self.text_area.get_buffer()
+        final = buffer.get_end_iter()
+        buffer.insert(final, texto)
+
+    def on_combo_changed(self, combo):
+        punteiro= combo.get_active_iter()
+
+        if  punteiro is not None:
+            modelo=combo.get_model()
+            elemento=modelo [punteiro][1]
+            bufer = self.text_area.get_buffer()
+            bufer.insert(bufer.get_end_iter(),"\nSeleccionado o elemento "+elemento + " do ComboBox")
 
 
 if __name__ == "__main__":
